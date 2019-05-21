11dbe5e801b96954fe0bdd1083c93b73819e3176
ash <ash@umoit.com>
2019-05-21 17:40:52 +0800

    444

--- resources/views/frontend/tracking.blade.php
+++ resources/views/frontend/tracking.blade.php
@@ -43,7 +43,7 @@
     <form method="post">
       <div class="form-group">
         <div class="input-group">
-          <div class="input-group-addon" style="padding:0px"><img height="50px;" src="/frontend/img/plane.jpg"></div>
+          <div class="input-group-addon" style="padding:0px;background:#fff"><img  src="/frontend/img/plane.jpg"></div>
           <input type="text" name="track_no" class="form-control">
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
            <span class="input-group-btn">
